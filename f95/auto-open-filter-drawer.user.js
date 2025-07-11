7b2f994ebe766d05d41897a186685574bee8491c
Edexaal <220395190+Edexaal@users.noreply.github.com>
2025-07-11 17:42:32 -0500

    merge

---
+++ f95/auto-open-filter-drawer.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name        F95 Auto Open Filter Drawer
+// @namespace   1330126-edexal
+// @match       *://f95zone.to/sam/latest_alpha/*
+// @grant       none
+// @icon        https://external-content.duckduckgo.com/ip3/f95zone.to.ico
+// @license     Unlicense
+// @version     1.2
+// @author      Edexal
+// @description Automatically open the filter drawer when visiting the Latest Update page.
+// @homepageURL https://sleazyfork.org/en/scripts/541896-f95-auto-open-filter-drawer
+// @supportURL  https://github.com/Edexal/monkey-scripts/issues
+// ==/UserScript==
+(() => {
+  function openDrawer() {
+    const observer = new MutationObserver(() => {
+      const drawerButton = document.querySelector('#controls_filter-toggle');
+      if (drawerButton) {
+        drawerButton.click();
+        observer.disconnect();
+      }
+    });
+    observer.observe(document.querySelector('#latest-page_sub-nav'), {
+      subtree: true,
+      childList: true
+    });
+  }
+
+  openDrawer();
+})();
