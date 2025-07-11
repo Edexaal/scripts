bda67936378552a1f029ab4f2f03417ca15cbb30
Edexaal <220395190+Edexaal@users.noreply.github.com>
2025-07-11 16:49:50 -0500

    update workflow

---
+++ README.md
@@ -0,0 +1,5 @@
+# Scripts
+My collection of userscripts. 
+
+## License
+All userscripts here are subject to the [Unlicense license](./LICENSE).
---
+++ _lib/utility.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        Edexal Utility Library
+// @namespace   1330126-edexal
+// @icon        -
+// @license     Unlicense
+// @version     1.0
+// @author      Edexal
+// @description Utility library for common reusable tasks
+// ==/UserScript==
+const edexal = {
+  //Apply custom styles in a style tag
+  applyCSS(css) {
+    let styleEl = document.querySelector("style");
+    if (styleEl === null) {
+      styleEl = document.createElement('style');
+    }
+    styleEl.appendChild(document.createTextNode(css));
+    document.head.appendChild(styleEl);
+  }
+}
