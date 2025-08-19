e58676502be023f40293ccaf720a1a83d2865e6f
Edexaal <220395190+Edexaal@users.noreply.github.com>
2025-08-19 14:06:13 -0500

    - Update adds new utilities - Change namespace from an object to a class - Adds a file to test libraries

--- _lib/utility.js
+++ _lib/utility.js
@@ -1,14 +1,13 @@
 // ==UserScript==
-// @name        Edexal Utility Library
+// @name        Edexal's Utility Library
 // @namespace   1330126-edexal
 // @license     Unlicense
-// @version     1.1
+// @version     2.0
 // @author      Edexal
 // @description Utility library for common reusable tasks
 // ==/UserScript==
-const edexal = {
-  //Apply custom styles in a style tag
-  applyCSS(css) {
+class Edexal {
+  static addCSS(css) {
     let styleEl = document.querySelector("style");
     if (styleEl === null) {
       styleEl = document.createElement('style');
@@ -16,5 +15,29 @@ const edexal = {
     styleEl.appendChild(document.createTextNode(css));
     document.head.appendChild(styleEl);
   }
-  //Test
+
+  static newEl(elObj) {
+    if (!Object.hasOwn(elObj, 'element')) return;
+    const el = document.createElement(elObj.element);
+    const {element, ...otherObjs} = elObj;
+    for (const [key, val] of Object.entries(otherObjs)) {
+      switch (key) {
+        case 'class':
+          el.classList.add(...val);
+          break;
+        case 'text':
+          const txt = document.createTextNode(val);
+          el.append(txt);
+          break;
+        default:
+          el.setAttribute(key, val);
+          break;
+      }
+    }
+    return el;
+  }
+
+  static onEv(el, eventType, callback, options) {
+    el.addEventListener(eventType, callback, options);
+  }
 }
