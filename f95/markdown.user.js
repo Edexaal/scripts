012b92cdc4e8b11aef8746f592f078ce42a4f82d
Edexaal <220395190+Edexaal@users.noreply.github.com>
2026-02-10 19:34:34 -0600

    Parse some basic markdown

--- f95/markdown.user.js
+++ f95/markdown.user.js
@@ -18,6 +18,61 @@
   //'<p><em>Hello </em></p><p><br></p><p><u>The</u> <s>names</s> the&nbsp;</p><p><br></p><p><strong><span style="font-size: 18px;">games</span></strong></p><p><br></p><p><a href="https://google.com" target="_blank" rel="noopener noreferrer">gesgesg</a></p>'
   // Posts button: div.formButtonGroup-primary > button:first-child
   // Threads button: div.formSubmitRow-controls > button:first-child
+  function submitEvent() {
+    const submitBtn = document.querySelector("div.formSubmitRow-controls > button:first-child")
+      || document.querySelector("div.formButtonGroup-primary > button:first-child");
+  }
+
+  function replace(line, regex, replacement) {
+    return line.replaceAll(regex,replacement);
+  }
+  function parseAdvanced(lineTxt) {
+    return lineTxt;
+  }
+  function parseIntermediate(lineTxt) {
+   return lineTxt;
+  }
+  function parseBasic(lineTxt) {
+    // Bold
+    lineTxt = replace(lineTxt,/\*\*(.+?)\*\*/gi,"<strong>$1</strong>");
+    // Italics
+    lineTxt = replace(lineTxt,/_(.+?)_|\*([^*]+?[^*])\*/gi,"<em>$1</em>");
+    // Strikethrough
+    lineTxt = replace(lineTxt,/~~(.+?)~~/gi,"<s>$1</s>");
+    // Link
+    lineTxt = replace(lineTxt, /\[(.+?)]\((.+?)\)/gi,'<a href="$2">$1</a>');
+    // Inline Code
+    lineTxt = replace(lineTxt, /`(.+?)`/gi,'<code>$1</code>');
+    return lineTxt;
+  }
+
+  function parseLine(lineTxt) {
+    lineTxt = parseBasic(lineTxt);
+    return lineTxt;
+  }
+
+  function parseMarkdown() {
+    const textBoxEl = document.querySelector("div.bbWrapper div[spellcheck][class*=fr-element]");
+    console.clear();
+    for (const lineEl of textBoxEl.children) {
+      if (lineEl.innerHTML === "<br>"){
+        continue;
+      }
+      // lineEl.outerHTML = "<p>" + parseLine(lineEl.innerHTML) + "</p>";
+      console.log("<p>" + parseLine(lineEl.innerHTML) + "</p>");
+    }
+  }
+  function createButton() {
+    const btnLayer = document.querySelector("div.formButtonGroup-primary,div.formSubmitRow-controls");
+    if (!btnLayer) return;
+    const btn = document.createElement('button');
+    btn.append(document.createTextNode("TEST"));
+    btn.addEventListener("click",parseMarkdown);
+    btnLayer.prepend(btn);
+  }
+  createButton();
+
+
   /*TODO: For Submit button
      1. Hide original submit and replace with custom button
      2. If clicked, transform markdown back into HTML format
@@ -30,6 +85,7 @@
       3. Swap buttons back
       4. Click preview of original button
       5. Repeat step 1 */
+  // Textbox: div.bbWrapper div[spellcheck][class*=fr-element]
   /*TODO: Parsing
      - Get text using textBoxEl.innerHTML
   *  - Loop through each markdown syntax type
