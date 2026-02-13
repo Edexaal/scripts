467fe1dc580b11b0e57e90e1eee36b92831d8747
Edexaal <220395190+Edexaal@users.noreply.github.com>
2026-02-13 00:11:46 -0600

    - Update description - Parsing is no longer line-based. - Make button a custom type

--- f95/markdown.user.js
+++ f95/markdown.user.js
@@ -8,13 +8,20 @@
 // @license     Unlicense
 // @version     0.1.0
 // @author      Edexal
-// @description Create posts and threads in markdown.
+// @description Create posts and threads using markdown syntax.
 // @homepageURL -
 // @supportURL  https://github.com/Edexaal/scripts/issues
 // @require     https://cdn.jsdelivr.net/gh/Edexaal/scripts@e58676502be023f40293ccaf720a1a83d2865e6f/_lib/utility.js
 // ==/UserScript==
 (async () => {
   const textBoxEl = document.querySelector('div.fr-element');
+  const formats = {
+    bold: {
+      tagIndex: 0,
+      tags: ["strong"],
+      regex: /\*\*/,
+    }
+  };
   //'<p><em>Hello </em></p><p><br></p><p><u>The</u> <s>names</s> the&nbsp;</p><p><br></p><p><strong><span style="font-size: 18px;">games</span></strong></p><p><br></p><p><a href="https://google.com" target="_blank" rel="noopener noreferrer">gesgesg</a></p>'
   // Posts button: div.formButtonGroup-primary > button:first-child
   // Threads button: div.formSubmitRow-controls > button:first-child
@@ -23,31 +30,29 @@
       || document.querySelector("div.formButtonGroup-primary > button:first-child");
   }
 
-  function replace(line, regex, replacement) {
-    return line.replaceAll(regex,replacement);
-  }
-  function parseAdvanced(lineTxt) {
-    return lineTxt;
-  }
-  function parseIntermediate(lineTxt) {
-   return lineTxt;
-  }
-  function parseBasic(lineTxt) {
-    // Bold
-    lineTxt = replace(lineTxt,/\*\*(.+?)\*\*/gi,"<strong>$1</strong>");
-    // Italics
-    lineTxt = replace(lineTxt,/_(.+?)_|\*([^*]+?[^*])\*/gi,"<em>$1</em>");
-    // Strikethrough
-    lineTxt = replace(lineTxt,/~~(.+?)~~/gi,"<s>$1</s>");
-    // Link
-    lineTxt = replace(lineTxt, /\[(.+?)]\((.+?)\)/gi,'<a href="$2">$1</a>');
-    // Inline Code
-    lineTxt = replace(lineTxt, /`(.+?)`/gi,'<code>$1</code>');
+  function inlineParse(lineTxt, format) {
+    while (lineTxt.search(format.regex) !== -1) {
+      if (!format.tagIndex) {
+        lineTxt = lineTxt.replace(format.regex, `<${format.tags[0]}>`);
+        format.tagIndex = 1;
+      } else {
+        lineTxt = lineTxt.replace(format.regex, `</${format.tags[0]}>`);
+        format.tagIndex = 0;
+      }
+    }
+    // Make sure there's an equal number of open to closed tag ratio.
+    let openTagsAmount = lineTxt.replaceAll(`<${format.tags[0]}>`, '@A@').match(/@A@/g)?.length ?? 0;
+    let closedTagsAmount = lineTxt.replaceAll(`</${format.tags[0]}>`, '@B@').match(/@B@/g)?.length ?? 0;
+    if (openTagsAmount > closedTagsAmount) {
+      lineTxt = `${lineTxt}</${format.tags[0]}>`;
+    } else if (closedTagsAmount > openTagsAmount){
+      lineTxt = `<${format.tags[0]}>${lineTxt}`;
+    }
     return lineTxt;
   }
 
-  function parseLine(lineTxt) {
-    lineTxt = parseBasic(lineTxt);
+  function parse(lineTxt) {
+    lineTxt = inlineParse(lineTxt, formats["bold"]);
     return lineTxt;
   }
 
@@ -55,23 +60,26 @@
     const textBoxEl = document.querySelector("div.bbWrapper div[spellcheck][class*=fr-element]");
     console.clear();
     for (const lineEl of textBoxEl.children) {
-      if (lineEl.innerHTML === "<br>"){
+      if (lineEl.innerHTML === "<br>") {
         continue;
       }
-      // lineEl.outerHTML = "<p>" + parseLine(lineEl.innerHTML) + "</p>";
-      console.log("<p>" + parseLine(lineEl.innerHTML) + "</p>");
+      lineEl.outerHTML = "<p>" + parse(lineEl.innerHTML) + "</p>";
+      // DEBUG: Writes results to dev console
+      // console.log("<p>" + parseLine(lineEl.innerHTML) + "</p>");
     }
   }
+
   function createButton() {
     const btnLayer = document.querySelector("div.formButtonGroup-primary,div.formSubmitRow-controls");
     if (!btnLayer) return;
     const btn = document.createElement('button');
     btn.append(document.createTextNode("TEST"));
-    btn.addEventListener("click",parseMarkdown);
+    btn.type = "button";
+    btn.addEventListener("click", parseMarkdown);
     btnLayer.prepend(btn);
   }
-  createButton();
 
+  createButton();
 
   /*TODO: For Submit button
      1. Hide original submit and replace with custom button
