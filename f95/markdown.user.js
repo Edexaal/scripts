d8c6760b89b70877fbc18352cebc7f9c256a06c0
Edexaal <220395190+Edexaal@users.noreply.github.com>
2026-02-14 01:14:55 -0600

    Some basic support for markdown syntax. - Adds support for italics, strikethrough, inline code, blockquote, and link - Add new parsing type

--- f95/markdown.user.js
+++ f95/markdown.user.js
@@ -15,14 +15,25 @@
 // ==/UserScript==
 (async () => {
   const textBoxEl = document.querySelector('div.fr-element');
+  function initFormat(tags, regex) {
+    return {tagIndex: 0, tags, regex};
+  }
+  function initSubFormat(regex,subRegex){
+    return {regex, subRegex};
+  }
+
   const formats = {
-    bold: {
-      tagIndex: 0,
-      tags: ["strong"],
-      regex: /\*\*/,
-    }
+    bold: initFormat(["strong"],/(?<!\\)\*\*/),
+    italic: initFormat(["em"], /(?<!\\)_/),
+    strikethrough: initFormat(["s"], /(?<!\\)~~/),
+    inlineCode: initFormat(["ICODE"], /(?<!\\)`/),
+    blockQuote: {
+      tags: ["QUOTE"],
+      regex: /^\s*(?:>|&gt;)\s*/,
+      indexes: []
+    },
+    link: initSubFormat(/\[(.+?)]\((.+?)\)/g,'<a href="$2">$1</a>'),
   };
-  //'<p><em>Hello </em></p><p><br></p><p><u>The</u> <s>names</s> the&nbsp;</p><p><br></p><p><strong><span style="font-size: 18px;">games</span></strong></p><p><br></p><p><a href="https://google.com" target="_blank" rel="noopener noreferrer">gesgesg</a></p>'
   // Posts button: div.formButtonGroup-primary > button:first-child
   // Threads button: div.formSubmitRow-controls > button:first-child
   function submitEvent() {
@@ -30,43 +41,98 @@
       || document.querySelector("div.formButtonGroup-primary > button:first-child");
   }
 
-  function inlineParse(lineTxt, format) {
+  // Substitute matches on a single line
+  function lineSubParse(lineTxt,format) {
+    if (lineTxt.search(format.regex) === -1) return lineTxt;
+    lineTxt = lineTxt.replace(format.regex,format.subRegex);
+    return lineTxt;
+  }
+
+  function quoteParse() {
+    const textBoxEl = document.querySelector("div.bbWrapper div[spellcheck][class*=fr-element]");
+    const format = formats["blockQuote"];
+    let count = 0;
+    for (let i = 0; i < format.indexes.length; i++){
+      // Removes `>`
+      textBoxEl.children[format.indexes[i]].outerHTML = `<p>${textBoxEl.children[format.indexes[i]].innerHTML.replace(format.regex,"")}</p>`;
+      if (format.indexes[i] + 1 !== format.indexes[i+1]){
+        if (count === 0) {
+          let lineTxt = textBoxEl.children[format.indexes[i]].innerHTML;
+          lineTxt = `[${format.tags[0]}]${lineTxt}[/${format.tags[0]}]`;
+          const pTag = document.createElement("p");
+          pTag.append(document.createTextNode(lineTxt));
+          textBoxEl.children[format.indexes[i]].replaceWith(pTag);
+        } else {
+          let lineTxt = textBoxEl.children[format.indexes[i - count]].innerHTML;
+          lineTxt = `[${format.tags[0]}]${lineTxt}`;
+          textBoxEl.children[format.indexes[i - count]].outerHTML = `<p>${lineTxt}</p>`;
+          lineTxt = textBoxEl.children[format.indexes[i]].innerHTML;
+          lineTxt = `${lineTxt}[/${format.tags[0]}]`;
+          textBoxEl.children[format.indexes[i]].outerHTML = `<p>${lineTxt}</p>`;
+        }
+        count = 0;
+      } else {
+        count++;
+      }
+    }
+    formats["blockQuote"].indexes.length = 0;
+  }
+  function quoteInfoGather(lineTxt,format,i) {
+    if (lineTxt.search(format.regex) !== -1) {
+      format.indexes.push(i);
+    }
+  }
+
+  function multiLineParse(lineTxt, format,startBracket,endBracket) {
     while (lineTxt.search(format.regex) !== -1) {
       if (!format.tagIndex) {
-        lineTxt = lineTxt.replace(format.regex, `<${format.tags[0]}>`);
+        lineTxt = lineTxt.replace(format.regex, `${startBracket}${format.tags[0]}${endBracket}`);
         format.tagIndex = 1;
       } else {
-        lineTxt = lineTxt.replace(format.regex, `</${format.tags[0]}>`);
+        lineTxt = lineTxt.replace(format.regex, `${startBracket}/${format.tags[0]}${endBracket}`);
         format.tagIndex = 0;
       }
     }
     // Make sure there's an equal number of open to closed tag ratio.
-    let openTagsAmount = lineTxt.replaceAll(`<${format.tags[0]}>`, '@A@').match(/@A@/g)?.length ?? 0;
-    let closedTagsAmount = lineTxt.replaceAll(`</${format.tags[0]}>`, '@B@').match(/@B@/g)?.length ?? 0;
+    let openTagsAmount = lineTxt.replaceAll(`${startBracket}${format.tags[0]}${endBracket}`, '@A@').match(/@A@/g)?.length ?? 0;
+    let closedTagsAmount = lineTxt.replaceAll(`${startBracket}/${format.tags[0]}${endBracket}`, '@B@').match(/@B@/g)?.length ?? 0;
     if (openTagsAmount > closedTagsAmount) {
-      lineTxt = `${lineTxt}</${format.tags[0]}>`;
-    } else if (closedTagsAmount > openTagsAmount){
-      lineTxt = `<${format.tags[0]}>${lineTxt}`;
+      lineTxt = `${lineTxt}${startBracket}/${format.tags[0]}${endBracket}`;
+    } else if (closedTagsAmount > openTagsAmount) {
+      lineTxt = `${startBracket}${format.tags[0]}${endBracket}${lineTxt}`;
     }
     return lineTxt;
   }
+  function iHTMLParse(lineTxt,format) {
+    return multiLineParse(lineTxt,format,'<','>');
+  }
+  function iBBCParse(lineTxt,format){
+    return multiLineParse(lineTxt,format,'[',']');
+  }
 
-  function parse(lineTxt) {
-    lineTxt = inlineParse(lineTxt, formats["bold"]);
+  function parse(lineTxt,i) {
+    lineTxt = iHTMLParse(lineTxt, formats["bold"]);
+    lineTxt = iHTMLParse(lineTxt, formats["italic"]);
+    lineTxt = iHTMLParse(lineTxt, formats["strikethrough"]);
+    lineTxt = iBBCParse(lineTxt,formats["inlineCode"]);
+    quoteInfoGather(lineTxt,formats["blockQuote"],i);
+    lineTxt = lineSubParse(lineTxt,formats["link"]);
     return lineTxt;
   }
 
   function parseMarkdown() {
     const textBoxEl = document.querySelector("div.bbWrapper div[spellcheck][class*=fr-element]");
     console.clear();
-    for (const lineEl of textBoxEl.children) {
+    for (let i = 0; i < textBoxEl.children.length; i++) {
+      const lineEl = textBoxEl.children[i];
       if (lineEl.innerHTML === "<br>") {
         continue;
       }
-      lineEl.outerHTML = "<p>" + parse(lineEl.innerHTML) + "</p>";
+      lineEl.outerHTML = "<p>" + parse(lineEl.innerHTML,i) + "</p>";
       // DEBUG: Writes results to dev console
       // console.log("<p>" + parseLine(lineEl.innerHTML) + "</p>");
     }
+    quoteParse();
   }
 
   function createButton() {
