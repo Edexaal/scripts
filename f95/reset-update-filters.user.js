463c78ea9869c19e0a65649faa294d013c2f651e
Edexaal <220395190+Edexaal@users.noreply.github.com>
2026-07-24 13:04:35 -0500

    feat: new user script

---
+++ f95/reset-update-filters.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Reset Update Filters
+// @namespace   1330126-edexal
+// @match       *://f95zone.to/sam/latest_alpha/*
+// @grant       none
+// @icon        https://external-content.duckduckgo.com/ip3/f95zone.to.ico
+// @license     Unlicense
+// @version     0.1.0
+// @author      Edexal
+// @description Reset individual filters in the filter drawer on the latest update page.
+// @homepageURL -
+// @supportURL  https://github.com/Edexaal/scripts/issues
+// @require     -
+// ==/UserScript==
+(async() =>{
+
+})()
